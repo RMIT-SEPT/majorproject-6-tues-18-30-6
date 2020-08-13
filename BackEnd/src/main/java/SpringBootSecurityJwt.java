dc99ec21884b2196f97036605eff2a89d68a953d
LiangyuNie S3716113 <s3716113@student.rmit.eud.au>
2020-08-14 03:19:04 +1000

    add userlogin front backend

---
+++ BackEnd/src/main/java/SpringBootSecurityJwt.java
@@ -0,0 +1,13 @@
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJwt {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJwt.class, args);
+	}
+
+}
