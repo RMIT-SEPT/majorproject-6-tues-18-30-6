f2c4e323c1fb104a54c0a489326999de871508d4
Sami Yildirim <professoryildirim@yahoo.com.au>
2020-08-16 17:59:02 +1000

    Reorganised File System
    Merged two backend projects for business and user registration

--- BackEnd/src/main/java/com/example/backend/model/EBusinessType.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.model;
-
-public enum EBusinessType {
-    TYPE_FOOD,
-    TYPE_MEDICAL,
-    TYPE_FINANCE,
-    TYPE_COSMETIC,
-    TYPE_OTHER
-}
---
+++ BackEnd/src/main/java/com/sept/userlogin/models/business/EBusinessType.java
@@ -0,0 +1,9 @@
+package com.sept.userlogin.models.business;
+
+public enum EBusinessType {
+    TYPE_FOOD,
+    TYPE_MEDICAL,
+    TYPE_FINANCE,
+    TYPE_COSMETIC,
+    TYPE_OTHER
+}
