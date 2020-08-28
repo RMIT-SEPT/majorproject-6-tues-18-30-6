e41d565be5537eb1191729c6b827023214983aaa
MichealK <37426227+MichealK@users.noreply.github.com>
2020-08-28 20:44:00 +1000

    Revert "Add files via upload"

--- index.js
+++ index.js
@@ -1,80 +1,73 @@
-//Note: this is to install the datepicker package- npm install react-datepicker --save 
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import './style.css';
-
-import DatePicker from "react-datepicker";
-
-import "react-datepicker/dist/react-datepicker.css";
-
-
-  function App () {
-  const [selectedDate, setSelectedDate] = useState(null)
-
-  
-  function mySubmitHandler(e) {
-   
-    if (document.getElementById("Datee").value === "") {
-      
-      alert("A date must be selected");
-      e.preventDefault();
-    }
-
-    if (document.getElementById("Timee").value === "") {
-      
-      alert("A time must be selected");
-      e.preventDefault();
-    }
-    
- 
-  };
-  
-  
-  return(
-    <form method="get" onSubmit={mySubmitHandler}>
-        
-        
-        <h2> Select a date, time combination which is avaliable:</h2>
-        <label>
-          Please pick a date where you're avaliable:
-          <br></br>
-         
-
-          <DatePicker id="Datee" name="date"
-          placeholderText="Click to select a date"
-        selected={selectedDate}
-        onChange={date => setSelectedDate(date)}
-        dateFormat='dd/MM/yyyy'
-        
-        minDate={new Date()}
-      />
-
-        </label>
-        <br></br>
-        <label>
-        Please select an avaliable time:
-        <select name="time" id="Timee">
-        <option value="">Select</option>
-            <option value="9:30 AM">9:30 AM</option>
-            <option value="10:30 AM">10:30 AM</option>
-            <option value="11:30 AM">11:30 AM</option>
-            <option value="12:30 PM">12:30 PM</option>
-          </select>
-          </label>
-        <br></br>
-        <input type="submit" value="Confirm Booking" />
-      </form>
-    
-  )
-  
-
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businessName: '',
+      abn: null,
+      category: null,
+    };
   }
 
-  
- 
+  mySubmitHandler = (event) => {
+    let abn = this.state.abn;
+    let businessName = this.state.businessName;
+    let category = this.state.category;
+    if (!Number(abn) || abn.length!=11){
+      alert("Your ABN must be a number and be 11 digits long");
+      event.preventDefault();
+    }
+    if (businessName==''){
+      alert("Business name can't be empty")
+      event.preventDefault();
+    }
+    if (category==null){
+      alert("Business category must be selected")
+      event.preventDefault();
+    }  
+  }
 
- 
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+  }
 
+  render() {
+    return (
+      <form method="get" onSubmit={this.mySubmitHandler}>
+      <h1>Business Registration Page </h1>
+      <p>Enter your Business Name:</p>
+      <input
+        type='text'
+        name='businessName'
+        onChange={this.myChangeHandler}
+        placeholder='Enter your business name'
+      />
+      <p>Enter your ABN:</p>
+      <input
+        type='text' name='abn'
+        title="An ABN is 11 digits long"
+        onChange={this.myChangeHandler}
+        placeholder='ABN must be 11 digits'
+      />
+      <p>Select your type of business category:</p>
+      <select name="category" onChange={this.myChangeHandler}>
+      <option value="" name="category">Select Category</option>
+    <option value="Lodging" name="category">Lodging</option>
+    <option value="Food and Drink" name="category">Food and Drink</option>
+    <option value="Event Planning" name="category">Event Planning</option>
+    <option value="Personal Care" name="category">Personal Care</option>
+    
+  </select>
+      <br/>
+      <br/>
+      <input type='submit' />
+      </form>
+    );
+  }
+}
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<MyForm />, document.getElementById('root'));
