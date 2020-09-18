e5d01da38dceb02d89d6739caa579b8dc91ecfc5
Sami Yildirim <professoryildirim@yahoo.com.au>
2020-09-18 15:41:06 +1000

    Label changes

--- FrontEnd/src/components/user-edit-details.component.js
+++ FrontEnd/src/components/user-edit-details.component.js
@@ -218,7 +218,7 @@ export default class EditDetails extends Component {
                 </div>
 
                 <div className="form-group">
-                  <button className="btn btn-primary btn-block">Sign Up</button>
+                  <button className="btn btn-primary btn-block">Confirm Details</button>
                 </div>
               </div>
             )}
