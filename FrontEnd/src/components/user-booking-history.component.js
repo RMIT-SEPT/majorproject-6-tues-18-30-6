58268158ce767e2c232d313881dbe1c218a95d9d
Sami Yildirim <professoryildirim@yahoo.com.au>
2020-09-21 18:12:03 +1000

    Changes to bookingHistory tab

--- FrontEnd/src/components/user-booking-history.component.js
+++ FrontEnd/src/components/user-booking-history.component.js
@@ -3,7 +3,7 @@ import AuthService from "../services/auth.service";
 import UserService from "../services/user.service";
 
 export default class BookingHistory extends React.Component {
-constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -12,37 +12,43 @@ constructor(props) {
     };
   }
 
-    componentDidMount() {
-      console.log("Getting Booking History")
-        UserService.getBookingHistory(AuthService.getCurrentUser()).then(
-          response => {
-            this.setState({
-              bookingHistory: response.data
-            });
-            console.log(response)
-          },
-          error => {
-            this.setState({
-              content:
-                (error.response &&
-                  error.response.data &&
-                  error.response.data.message) ||
-                error.message ||
-                error.toString()
-            });
-          }
-        );
+  componentDidMount() {
+    console.log("Getting Booking History")
+    UserService.getBookingHistory(AuthService.getCurrentUser()).then(
+      response => {
+        this.setState({
+          bookingHistory: response.data
+        });
+        console.log(response)
+      },
+      error => {
+        this.setState({
+          content:
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString()
+        });
       }
-    render() {
-        return (
-          <div>
-          <h2>Booking History</h2>
-          {  this.state.bookingHistory.map((booking) => (
-              <li key={booking.id}>Date: {booking.date}, # of Passengers: {booking.numPassengers}</li>
-           ))
-        
-          }
-          </div>
-        );
+    );
+  }
+  render() {
+    let display = true;
+    if (this.state.bookingHistory.length === 0) {
+      display = false;
     }
+
+    return (
+      <div>
+        <h2>Booking History</h2>
+        {
+          display ? (
+            this.state.bookingHistory.map((booking) => (
+              <li key={booking.id}>Date: {booking.date}, Business: {booking.business}, Worker: {booking.worker}</li>
+            ))) : ("No Bookings Found")
+        }
+      </div>
+    );
+  }
 }
